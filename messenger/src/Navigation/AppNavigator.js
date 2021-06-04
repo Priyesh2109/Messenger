361cd50438c8b5b3c363e6ae9d1b31697da9c661
priyesh <priyeshpatel9843516@icloud.com>
2021-06-04 11:24:17 +0100

    Add: created tabs for different screens

--- messenger/src/Navigation/AppNavigator.js
+++ messenger/src/Navigation/AppNavigator.js
@@ -1,12 +1,18 @@
 import React from "react"
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
 import SettingsNavigator from "./SettingsNavigator"
+import CameraNavigator from "./CameraNavigator"
+import ChatNavigator from "./ChatsNavigator"
+import CallNavigator from "./CallsNavigator"
 
 const Tab = createBottomTabNavigator()
 
 const AppNavigator = () => (
   <Tab.Navigator>
+    <Tab.Screen name="Calls" component={CallNavigator} />
+    <Tab.Screen name="Camera" component={CameraNavigator} />
+    <Tab.Screen name="Chats" component={ChatNavigator} />
     <Tab.Screen name="Settings" component={SettingsNavigator} />
   </Tab.Navigator>
 )
